b3b77fe90896078910c6c3ba32c8a78ac94e2e69
Kate <k.dihich@gmail.com>
2024-08-16 16:24:27 +0100

    filter url done

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -3,13 +3,14 @@ import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
 
 import AddCabin from "../features/cabins/AddCabin";
+import CabinTableOperations from "../features/cabins/CabinTableOperation";
 
 function Cabins() {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All Cabins</Heading>
-        <p>Filter / Sort</p>
+        <CabinTableOperations />
       </Row>
 
       <Row type="vertical">
